31c77b6a091deedec25138edee0331f9d64f5ca8
minm063 <jangmin063@gmail.com>
2024-02-13 22:15:51 +0900

    feat: test3

--- src/main/java/com/example/jenkinstest/controller/TestController.java
+++ src/main/java/com/example/jenkinstest/controller/TestController.java
@@ -21,4 +21,9 @@ public class TestController {
     public String test2() {
         return "test2";
     }
+    @GetMapping("/test3")
+    public String test3() {
+        return "test2";
+    }
+
 }
